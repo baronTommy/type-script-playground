55990aa2c7b41bc9504d4784a906f63cd5fc2d33
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-07-23 23:54:41 +0900

    fmt

--- src/ch2/index.test.ts
+++ src/ch2/index.test.ts
@@ -1,128 +1,123 @@
-describe("ch2", () => {
+it("unknown", () => {
+  // unknown 型安全な any
+  const x: unknown = "unknown";
 
-  it("unknown", () => {
-    // unknown 型安全な any
-    const x: unknown = "unknown";
+  // このタイミングだと lengthは NG
+  // expect(x.length).toBe('unknown'.length)
+  if (typeof x === "string") {
+    // ここで 型を確定 させると length OK
+    expect(x.length).toBe("unknown".length);
+  }
+});
 
-    // このタイミングだと lengthは NG
-    // expect(x.length).toBe('unknown'.length)
-    if (typeof x === "string") {
-      // ここで 型を確定 させると length OK
-      expect(x.length).toBe("unknown".length);
+it("nevar", () => {
+  // voidとの違いは、関数が正常に終了して値が返ってくるということが無い
+  const x = (p: boolean): never | string => {
+    if (p) {
+      return "OK";
     }
-  });
+    throw new Error("never");
+  };
+  expect(x(true)).toBe("OK");
 
-  it("nevar", () => {
-    // voidとの違いは、関数が正常に終了して値が返ってくるということが無い
-    const x = (p: boolean): never | string => {
-        if (p) {
-            return 'OK'
-        }
-        throw new Error('never')
-    }
-    expect(x(true)).toBe('OK')
+  // これが void
+  const x2 = (): void => {
+    return;
+  };
+  expect(x2()).toBe(undefined);
+});
 
-    // これが void
-    const x2 = (): void => {
-        return
-    }
-    expect(x2()).toBe(undefined)
-  });
+it("Intersection Type", () => {
+  type User = {
+    name: string;
+    age: number;
+  };
 
-  it('Intersection Type', () => {
-    type User = {
-        name: string
-        age: number
-    }
+  type Robot = {
+    switch: "on" | "off";
+  };
 
-    type Robot = {
-        switch: 'on' | 'off'
-    }
+  type Humanoid = User & Robot;
 
-    type Humanoid = User & Robot
+  const droid: Humanoid = {
+    name: "どろいど",
+    age: 0,
+    switch: "on"
+  };
 
-    const droid: Humanoid = {
-        name: 'どろいど',
-        age: 0,
-        switch: 'on'
-    }
+  expect(droid).toBeDefined();
+});
 
-    expect(droid).toBeDefined()
-  })
+it("Symbol", () => {
+  // const a = Symbol()
+  // const b = Symbol()
+  // Symbolは、 a === b にならない
+  // objectのkeyとして使うと良さげ？
 
-  it('Symbol', () => {
-    // const a = Symbol()
-    // const b = Symbol()
-    // Symbolは、 a === b にならない
-    // objectのkeyとして使うと良さげ？
+  const a = Symbol();
 
-    const a = Symbol()
+  expect(typeof a === "symbol").toBe(true);
+});
 
-    expect(typeof a === 'symbol').toBe(true)
-  })
+it("Union Type", () => {
+  const v: number | string = "123";
+  expect(v).toBeDefined();
 
-  it('Union Type', () => {
-    const v: number | string = '123'
-    expect(v).toBeDefined()
+  const v2: (number | string)[] = [1, "2", 3];
+  expect(v2).toBeDefined();
+});
 
-    const v2: (number | string)[] = [1, '2', 3]
-    expect(v2).toBeDefined()
-  })
+it("Literal Type", () => {
+  const v: "a" | "b" = "a";
+  expect(v).toBeDefined();
+});
 
-  it('Literal Type', () => {
-    const v: 'a' | 'b' = 'a'
-    expect(v).toBeDefined()
-  })
+it("Numeric Literal Type", () => {
+  const v: 0 | 5 = 5;
+  expect(v).toBeDefined();
+});
 
-  it('Numeric Literal Type', () => {
-    const v: 0 | 5 = 5
-    expect(v).toBeDefined()
-  })
+it("typeof", () => {
+  const myName = "foo";
 
-  it('typeof', () => {
-    const myName = 'foo'
+  // typeof のテク 変数を指定できる
+  // @ts-ignore
+  const friendName: typeof myName;
 
-    // typeof のテク 変数を指定できる
-    // @ts-ignore
-    const friendName: typeof myName
+  // @ts-ignore
+  expect(typeof friendName === "string");
+});
 
-    // @ts-ignore
-    expect(typeof friendName === 'string')
-  })
+it("keyof", () => {
+  type SomeType = {
+    foo: string;
+    bar: string;
+  };
 
-  it('keyof', () => {
-      type SomeType = {
-          foo: string
-          bar: string
-      }
+  // foo or bar みたいな限定が可能
+  const someKey: keyof SomeType = "foo";
+  expect(someKey === "foo" || "bar").toBe(true);
+});
 
-      // foo or bar みたいな限定が可能
-      const someKey: keyof SomeType = 'foo'
-      expect(someKey === 'foo' || 'bar').toBe(true)
-  })
+it("keyof と typeof の併用", () => {
+  // objectのkeyのみしか受け付けない
 
-  it('keyof と typeof の併用', () => {
-      // objectのkeyのみしか受け付けない
+  const x = {
+    foo: "aaa",
+    bar: "bbb"
+  };
 
-      const x = {
-          foo: 'aaa',
-          bar: 'bbb'
-      }
+  let y: keyof typeof x = "foo";
+  expect(y).toBe("foo");
+});
 
-      let y: keyof typeof x = 'foo'
-      expect(y).toBe('foo')
-  })
+it("enum", () => {
+  // enum は アッパーケースが良いらしい
+  enum Direction {
+    Up,
+    Down
+    // ...
+  }
 
-  it('enum', () => {
-      // enum は アッパーケースが良いらしい
-      enum Direction {
-          Up,
-          Down,
-          // ...
-      }
-
-      expect(Direction.Up).toBe(Direction.Up)
-
-  })
-
+  expect(Direction.Up).toBe(Direction.Up);
 });
