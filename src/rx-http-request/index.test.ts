f5cdff1bd7127911f67d973434a673a6dbe4ecf2
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-07-28 13:08:40 +0900

    rxjs

--- src/rx-http-request/index.test.ts
+++ src/rx-http-request/index.test.ts
@@ -1,15 +1,19 @@
-// import { RxHR } from "@akanass/rx-http-request";
+import { RxHR } from "@akanass/rx-http-request";
+import { tap } from "rxjs/operators";
+import { Observer } from "rxjs";
+
+function fetData() {
+  return RxHR.get("https://api.zipaddress.net/")
+    .pipe(
+      tap({
+        next: () => console.log("ok"),
+        error: () => console.log("error"),
+        complete: () => console.log("complete")
+      })
+    )
+    .subscribe();
+}
 
 it("aa", () => {
-
-
-//   RxHR.get("https://api.zipaddress.net/").subscribe(
-//     r => {
-//       console.log(r.response.statusCode);
-//     },
-//     err => console.error(err),
-//     () => console.log("fix")
-//   );
-
-
+  expect(true).toBe(true)
 });
--- src/rxjs/index.test.ts
+++ src/rxjs/index.test.ts
@@ -1,52 +1,75 @@
-import { of, throwError, concat } from "rxjs";
+import { of, Observer, Observable, identity, throwError, concat } from "rxjs";
 // Operators とは Observable に対してフィルターをかけるなど値を加工するためのメソッド群
-import { map, catchError, tap } from "rxjs/operators";
+import { map, catchError, tap, delay, finalize } from "rxjs/operators";
+import { deepStrictEqual } from "assert";
+import { TestScheduler } from "rxjs/testing";
 
-function log(msg: string) {
-  console.log(msg);
-}
+const log = (msg: string) => {};
+// logあると見づらいのでコメントに
+// console.log(msg);
 
-function errLog() {
-  log("errLog: ng");
-}
+const errorLog = (error: any) => log(error.message);
 
-function completeLog() {
-  log("complete");
-}
+const completeLog = () => console.log("on complete");
 
-function addToEnd(p: string) {
-  return (base: string) => base.concat(p);
-}
+const addToEnd = (p: string) => (base: string) => base.concat(p);
 
-function errorHandler(p: string) {
-  return of(p, "errorHandler");
-}
+const errorHandler = (p: string) => throwError("ya ba i");
 
-const data = [
-  of("a", "b", "c"),
-  throwError({message: "throwError"}),
-  of("x", "y", "z") // 上で throwErrorしているのでここは流れない
-];
+const mainEffect = ({ next, error, complete }: Observer<string>) => ({
+  next, // 新しい値が流れてきたときに
+  error, // ストリームの途中でエラーが発生したときに
+  complete // 全ての値が流れきってストリームが終了した時
+});
 
-it("rxjs pipeにcatchError有り of を利用して Observableを返す", () => {
-  // Observables 非同期データソース ( ストリーム ) を表すクラスで、言葉通り Observe ( 観察 ) する何か
+const epicLike = (resources$: Observable<string>) =>
+  resources$.pipe(
+    map(addToEnd("x")),
+    tap(mainEffect({ next: log, error: errorLog, complete: completeLog })),
+    catchError(errorHandler)
+  );
+
+it("正常系", () => {
+  const marble = "-a-b-c|";
+
+  const vales = {
+    a: "aa",
+    b: "bb",
+    c: "cc"
+  };
+
+  const expectedVales = {
+    a: "aax",
+    b: "bbx",
+    c: "ccx"
+  };
+
+  new TestScheduler(deepStrictEqual).run(({ hot, expectObservable }) => {
+    const input$ = hot(marble, vales);
+    expectObservable(epicLike(input$)).toBe(marble, expectedVales);
+  });
+});
+
+const fun = (p: Observable<string | any>) =>
+  p.pipe(
+    finalize(() => console.log("finalize")),
+    delay(1000),
+    map(identity),
+    catchError(() => throwError("yabai X"))
+  );
+
+it("異常系", () => {
+  const l = ["a", "b", "c"];
+  const data = [of(...l), throwError({ message: "throwError" })];
   const resources$ = concat(...data);
 
-  // pipe で事前に下ごしらえをして subscribeで流す
-  resources$
-    .pipe(
-      map(addToEnd("111")),
-      tap({
-        // 新しい値が流れてきたときに
-        next: log,
-        // ストリームの途中でエラーが発生したときに
-        error: error => {
-          log(error.message);
-        },
-        // 全ての値が流れきってストリームが終了した時
-        complete: () => console.log('on complete')
-      }),
-      catchError(errorHandler)
-    )
-    .subscribe(); // subscribe することで値が流れる
+  fun(resources$).subscribe(
+    r => {
+      expect(r).toBe(l.shift());
+    },
+    e => {
+      expect(e).toBe('yabai X')
+    },
+    () => console.log("fix")
+  );
 });
